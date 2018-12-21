b011af902332bc8397a7bb00c27f0c172fcdc412
Carolyns-Lagos <carolyns.echegaray@gmail.com>
2018-12-21 11:36:50 -0300

    Guia 16

---
+++ src/main/java/cl/accenture/programatufuturo/guias/dao/CancionDAO.java
@@ -0,0 +1,6 @@
+package cl.accenture.programatufuturo.guias.dao;
+
+public class CancionDAO {
+
+    cd "E:\\Carolyns\\Ejercicios y clases Java\\clases\\2018\\12 Diciembre\\20-12-18"
+}
--- src/main/java/cl/accenture/programatufuturo/guias/model/Cancion.java
+++ src/main/java/cl/accenture/programatufuturo/guias/model/Cancion.java
@@ -1,9 +1,10 @@
-package model;
+package cl.accenture.programatufuturo.guias.model;
 
 public class Cancion {
     private int idCancion, duracion;
     private String nombre, autor, genero;
 
+    //Constructores, siempre como minimo debo tener un contructor por defecto y uno que reciba todos los atributos
     public Cancion() {
         this.idCancion = 0;
         this.duracion = 0;
@@ -21,52 +22,72 @@ public class Cancion {
     }
 
     public int getIdCancion() {
+
         return this.idCancion;
     }
 
     public void setIdCancion(int idCancion) {
+
         this.idCancion = idCancion;
     }
 
     public int getDuracion() {
+
         return this.duracion;
     }
 
 
 
     public void setDuracion(int duracion) {
+
         this.duracion = duracion;
     }
 
     public String getNombre() {
+
         return this.nombre;
     }
 
     public void setNombre(String nombre) {
+
         this.nombre = nombre;
     }
 
     public String getAutor() {
+
         return autor;
     }
 
     public void setAutor(String autor) {
+
         this.autor = autor;
     }
 
     public String getGenero() {
+
         return genero;
     }
 
     public void setGenero(String genero) {
+
         this.genero = genero;
     }
 
-    public boolean equals(Object o){
-        if (o instanceof Cancion){
-            Cancion x=(Cancion) o;
-            this.getIdCancion()=x.getIdCancion();
+    public boolean equals(Object obj){
+        if (obj instanceof Cancion){
+            Cancion x=(Cancion) obj;
+            return this.getIdCancion()==x.getIdCancion();
+            //Estaria retornando verdadero si se cumple la primera condicion, siendo las canciones iguales segun el id
         }
         return false;
     }
+
+    public String toString(){
+        return this.idCancion + " - "+
+                this.getNombre()+ " - "+
+                this.getAutor()+" - "+
+                this.getGenero()+" - "+
+                this.getDuracion();
+    }
+    //Metodo que sera la forma en la que se imprimira.
 }
